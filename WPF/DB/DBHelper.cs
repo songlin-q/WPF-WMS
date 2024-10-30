f9940b46b2b652a41c00e5dba35d1f5da73cc779
松林 <442534979@qq.com>
2024-10-30 15:14:25 +0800

    添加项目文件。

---
+++ WPF/DB/DBHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem.DB
+{
+    /// <summary>
+    /// 数据库操作类
+    /// </summary>
+    public class DBHelper
+    {
+        public static DBHelper Instance { get; set; }
+    }
+}
---
+++ WPF/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           MessageBox.Show("单击事件");
+        }
+        /// <summary>
+        /// 登录事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("开始登录！");
+        }
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
+            //判定是否需要进行注册
+
+
+             MessageBox.Show( "开始注册！");
+        }
+    }
+}
